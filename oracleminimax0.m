dc50eb2cacdc742f63c6078d913ed61ce48ec4e0
Huang Wenjie <wayne9205@gmail.com>
2024-09-11 13:47:29 +0800

    Add files via upload

---
+++ oracleminimax0.m
@@ -0,0 +1,25 @@
+function [L]=oracleminimax0(x,n,b,data)
+D=2430;k=10^(-8);A=60;v=80;% Para
+s=x(1); l=x(2); t=x(3);
+y=zeros(1000,1);
+for i=1:1000
+    y(i)=i/10-0.1;
+end
+Recordy=zeros(1000,1);
+for j=1:1000
+    L1=0;i=1;
+    while i<=n-1
+        L1=L1+(s/(A*sqrt(4*pi*D*(data(i,1)-t))))*exp(-((data(i,2)-l-v*(data(i,1)-t))^2)/(4*D*(data(i,1)-t))-data(i,3))^2 ...
+            +y(j)*(exp(0.5*((s/(A*sqrt(4*pi*D*(data(i,1)-t))))*exp(-((data(i,2)-l-v*(data(i,1)-t))^2)/(4*D*(data(i,1)-t)))-data(i,3))^2)-b);
+        i=i+1;
+    end
+    Recordy(j)=L1;
+end
+y0=find( Recordy ==max( Recordy ));
+y1=y0(1)/10-0.1;
+L=0;i=1;
+while i<=n-1
+    L=L+(s/(A*sqrt(4*pi*D*(data(i,1)-t))))*exp(-((data(i,2)-l-v*(data(i,1)-t))^2)/(4*D*(data(i,1)-t))-data(i,3))^2 ...
+            +y1*(exp(0.5*((s/(A*sqrt(4*pi*D*(data(i,1)-t))))*exp(-((data(i,2)-l-v*(data(i,1)-t))^2)/(4*D*(data(i,1)-t)))-data(i,3))^2)-b);
+    i=i+1;
+end
